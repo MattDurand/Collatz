dc1b33053172eadcaa494b679487aa0a447d3b4c
MattDurand <matthewdurand27@gmail.com>
2019-10-08 21:41:12 +0100

    First draft fixed.

--- collatz.py
+++ collatz.py
@@ -6,11 +6,12 @@ n = 20
 while n != 1:
     # Print the current value of n.
     print(n)
-    if n % 2 = 0:
-        # If n is even, divide it by two.
+    if n % 2 == 0:
+        # If n is even, divide it by 2.
         n= n / 2
     else:
-        # If n is odd, multiply it by three and add one.
+        # If n is odd, multiply it by 3 and add 1.
         n = (3 * n) + 1
 
+#Finally, print the 1.
 print(n)
